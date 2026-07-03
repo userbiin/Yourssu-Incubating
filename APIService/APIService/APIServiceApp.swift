7622385c027de187be6e8a1ed73664c543c3c39a
userbiin <steph1125@soongsil.ac.kr>
2026-07-03 14:34:10 +0900

    [✨ Feat] 클래식 서치 앱

---
+++ APIService/APIService/APIServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIServiceApp.swift
+//  APIService
+//
+//  Created by Subin on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct APIServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
